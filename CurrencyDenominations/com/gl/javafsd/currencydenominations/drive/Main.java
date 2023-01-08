5624e8bf7ae5875f6315435431b16ca9adc3f900
BHARGAV REDDY R <116778632+BhargavReddyR@users.noreply.github.com>
2023-01-08 21:02:22 +0530

    Initial commit

---
+++ CurrencyDenominations/com/gl/javafsd/currencydenominations/drive/Main.java
@@ -0,0 +1,29 @@
+package com.gl.javafsd.currencydenominations.drive;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.gl.javafsd.currencydenominations.service.MergeSortImplementation;
+import com.gl.javafsd.currencydenominations.service.NotesCount;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		MergeSortImplementation mergeSortImplementation = new MergeSortImplementation();
+		NotesCount notesCount = new NotesCount();
+
+		System.out.println("enter the size of currency denominations ");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] notes = new int[size];
+		System.out.println("enter the currency denominations value");
+		for (int i = 0; i < size; i++) {
+			notes[i] = sc.nextInt();
+		}
+		System.out.println("enter the amount you want to pay");
+		int amount = sc.nextInt();
+		mergeSortImplementation.sort(notes,0,notes.length-1);
+		notesCount.notesCountImplementation(notes, amount);
+	}
+}
